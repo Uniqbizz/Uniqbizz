82845557bfed1338b151fb5b04f22f0ac39efd52
Uniqbizz <uniqbizzbizzmirth@gmail.com>
2025-11-13 14:20:22 +0530

    1.1 Updated the japan destination file.

--- destination-details/destination-details-japan.php
+++ destination-details/destination-details-japan.php
@@ -100,65 +100,58 @@
                             <div class="destination-details-content">
                                 <h4 class="title">Experience Japan - Where Every Sunrise Tells A Story</h4>
                                 <p class="pera">
-                                    DJapan is an East Asian nation located in the Pacific Ocean. It is made up of an archipelago, whose 
-                                    four largest Island include Honshu, Hokkaido, Kyushu, and Shikoku.  Tokyo is its capital city and 
-                                    the most populous city in the world. Tokyo is famous for having a wide variety of tourist 
-                                    attractions. The Imperial Palace East Gardens is a beautiful and historic location within the 
-                                    Tokyo Imperial Palace grounds. Chidorigafuchi Moat is Japan's most visited spot for cherry blossoms. 
-                                    Yasukuni Shrine pays homage to Japan's war heroes and is of great historical significance. 
-                                    Marunouchi is a thriving commercial area, famous for its high-rise skyscrapers and traditional 
-                                    landmarks that easily combine modernity and heritage.  
+                                    Japan is an East Asian country in the Pacific Ocean. It is composed of four main islands, namely, 
+                                    Honshu, Hokkaido, Kyushu, and Shikoku. Tokyo, the capital of Japan, is the most populous city in the 
+                                    world. It is renowned for a diverse range of tourist attractions. The Imperial Palace East Garden is 
+                                    beautiful and historic and is located within the grounds of the Imperial Palace in Tokyo. 
+                                    Chidorigafuchi Moat is one of the most popular cherry blossom destinations in Japan. Yasukuni Shrine 
+                                    is a landmark site that honours Japan's war heroes. Marunouchi is a well-known business area, famous 
+                                    for its towering buildings and historic sites, creating a blend of contemporary design and 
+                                    traditional charm.  
                                 </p>
                                 <p class="pera">
-                                    Hiroshim another popular tourist destination is a place where the atomic bomb was, dropped in 1945. 
-                                    We have two memorial sites to visit, the iconic Atomic Bomb Dome and the Peace memorial. Hiroshima 
-                                    Castle is a symbol of the city's rich samurai heritage.  The historical gardens of "Shukkei-en" 
-                                    which means, “shrunken scenery gardens” is located in Hiroshima. At midday, a tourist can savour 
-                                    a delicious Okonomiyaki lunch — Hiroshima’s signature savoury pancake layered with noodles and 
-                                    vegetables. 
+                                    Hiroshima is another major tourist attraction in Japan. It's the city where the atomic bomb was 
+                                    detonated in 1945. You can find two significant memorials here, namely the Atomic Bomb Dome and the 
+                                    Peace Memorial.  
                                 </p>
                                 <p class="pera">
-                                    A short ferry ride from Hiroshima will take you to Miyajima, a small shrine island renowned for 
-                                    the iconic Itsukushima Shrine, a "floating" tori gate, which is a UNESCO world heritage site. 
-                                    Tenshinkaku is a charming, traditional Japanese house turned café, located on a hill in Miyajima. 
-                                    You can enjoy the scenic view that includes the iconic Toyokuni Shrine, the elegant Five-Story 
-                                    Pagoda, and the tranquil waters of the Seto Inland Sea Kyoto, a former capital of Japan, is a city 
-                                    situated on the Honshu Island.  It's renowned for its many traditional Buddhist temples along with 
-                                    gardens, imperial palaces, Shinto shrines, and traditional wooden houses. It's also well known for 
-                                    its formal customs, including kaiseki dining, which is made up of various dishes, and geisha female 
-                                    entertainers.   
+                                    Hiroshima Castle is a symbol of the city's rich samurai heritage. The historical gardens of 
+                                    "Shukkei-en", which means "shrunken scenery gardens", are located in Hiroshima. At midday, a tourist 
+                                    can savour a delicious okonomiyaki lunch — Hiroshima’s signature savoury pancake layered with noodles 
+                                    and vegetables.   
                                 </p>
                                 <p class="pera">
-                                    Nara is home to eight UNESCO Heritage site namely Todai-ji Temple, Kohfuku-ji Temple, Kasuga-Taisha 
-                                    Shrine, Kasugayama Primeval Forest, Gango-ji Temple, Yakushi-ji Temple, Toshodai-ji Temple, and 
-                                    the Nara Palace Site, which is worth a visit. 
+                                    Kyoto, a former capital of Japan, is a city situated on the island of Honshu. It's renowned for its 
+                                    many traditional Buddhist temples along with gardens, imperial palaces, Shinto shrines, and 
+                                    traditional wooden houses. It's also well known for its formal customs, including kaiseki dining, 
+                                    which consists of various dishes, and geisha female entertainers. 
                                 </p>
                                 <p class="pera">
-                                    Osaka is a large commercial port of the Japanese. Osaka castle is an iconic landmark. It offers a 
-                                    glimpse into Japan’s feudal history. Universal Studios in Japan is a fun-packed theme park for all 
-                                    ages that attracts large crowds. Shitenno-ji is Japan’s oldest official Buddhist temple. Umeda Sky 
-                                    Building is a futuristic structure that is famous for its Floating Garden Observatory where visitors 
-                                    have to take the lift to the top and enjoy panoramic views of Osaka’s skyline—especially stunning 
-                                    at sunset or night. Osaka Aquarium Kaiyukan is one of the world’s largest aquariums, featuring sea 
-                                    creatures from across the Pacific Ocean. Sumiyoshi Taisha Shrine a Shinto shrine known for its 
-                                    unique architectural style and arched bridge.  There are also a wide variety of hidden food stalls 
-                                    and alleys and a tourist, must-tries the kushikatsu (deep-fried skewers) and negiyaki 
-                                    (green onion pancakes).   
+                                    Nara is home to eight UNESCO Heritage sites, namely Todai-ji Temple, Kohfuku-ji Temple, 
+                                    Kasuga-Taisha Shrine, Kasugayama Primaeval Forest, Gango-ji Temple, Yakushi-ji Temple, Toshodai-ji 
+                                    Temple, and the Nara Palace Site, all of which are worth a visit.  
+                                </p>
+                                <p class="pera">
+                                    Osaka is a major commercial port in Japan. Osaka Castle is an iconic landmark. It offers a glimpse 
+                                    into Japan’s feudal history. Universal Studios in Japan is a fun-packed theme park for all ages that 
+                                    attracts large crowds. Shitenno-ji is Japan’s oldest official Buddhist temple. 
+                                </p>
+                                <p class="pera">
+                                    The Umeda Sky Building is renowned for its Floating Garden Observatory, where visitors can take a 
+                                    lift to the summit and enjoy the panoramic view of Osaka’s skyline, which is particularly 
+                                    breathtaking at dusk or at night. The Osaka Aquarium Kaiyukan is one of the world's largest 
+                                    aquariums, showcasing a diverse array of marine life from all over the Pacific.   
+                                </p>
+                                <p class="pera">
+                                    Sumiyoshi Taisha Shrine is a Shinto shrine recognised for its distinctive architectural style and 
+                                    arched bridge. There are also a wide variety of hidden food stalls and alleys, and tourists must try 
+                                    the kushikatsu (deep-fried skewers) and negiyaki (green onion pancakes).    
                                 </p>
                                 <p class="pera">
                                     Whether you are exploring the cultural heart of Kyoto, climbing Mount Fuji, soaking in an onsen 
-                                    (hot spring), or enjoying sushi in Tokyo. Japan offers a blend of tradition and modernity, a 
-                                    high-tech experience of the world-renowned bullet train, all highlight Japan’s cultural legacy of 
-                                    ancient temples, and nightlife, filled with traditional food handed down from generations. 
-                                </p>
-                                <p class="pera">
-                                    Japan is a captivating blend of ancient tradition and cutting-edge modernity, offering travellers a 
-                                    truly unique experience. From serene temples and historic castles to cherry blossoms and neon-lit 
-                                    cities, the country seamlessly combines the past and the future, featuring high-speed trains and 
-                                    futuristic technology. Whether you're exploring the cultural heart of Kyoto, climbing Mount Fuji, 
-                                    soaking in an onsen (hot spring), or enjoying world-class sushi in Tokyo, Japan delights with its 
-                                    hospitality, cleanliness, and attention to detail, making it a must-visit destination for all types 
-                                    of travellers.   
+                                    (hot spring), or enjoying sushi in Tokyo. Japan offers a blend of tradition and modernity, featuring 
+                                    a high-tech experience with the world-renowned bullet train, all highlighting Japan’s cultural legacy 
+                                    of ancient temples and nightlife, filled with traditional food handed down through generations.    
                                 </p>
                             </div>
                             <!--/ details-content -->
