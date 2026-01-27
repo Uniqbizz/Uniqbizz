eda722badb2a5638acb8428ed0a1692dfdeb86e7
--global <saviovaz20@gmail.com>
2026-01-27 15:11:56 +0530

    uniqbizz 1.1 added upgrade history for franchisee overview (admin) --- need design fix

--- admin/overview_profile/overview.php
+++ admin/overview_profile/overview.php
@@ -11,7 +11,8 @@ $date = date('Y');
 
 $id = $_GET['id'];
 $ref = $_GET['ref'];
-
+$tamount='';
+$initial_inv='';
 $DBtable = $_GET['message'];
 $designation = $_GET['designation'];
 
@@ -38,10 +39,13 @@ else if ($DBtable == 'business_mentor') { // 26
 else if ($DBtable == 'master_franchisee') { // 28
     $sql = "SELECT * FROM master_franchisee WHERE master_franchisee_id = '" . $id . "' AND status = '1'";
 }
+else if ($DBtable == 'sponsor_franchisee') { // 30
+    $sql = "SELECT * FROM sponsor_franchisee WHERE sponsor_franchisee_id = '" . $id . "' AND status = '1'";
+}
 else if ($DBtable == 'sub_franchisee') { // 29
     $sql = "SELECT * FROM sub_franchisee WHERE sub_franchisee_id = '" . $id . "' AND status = '1'";
 }
-else if ($DBtable == 'zonal_manager') { // 29
+else if ($DBtable == 'zonal_manager') { // 27
     $sql = "SELECT * FROM zonal_manager WHERE zonal_manager_id = '" . $id . "' AND status = '1'";
 }else if ($DBtable == 'relationship_manager') { // 31
     $sql = "SELECT * FROM employees WHERE employee_id = '" . $id . "' AND status = '1' AND user_type=31";
@@ -144,6 +148,9 @@ if ($stmt->rowCount() > 0) {
             }
             
         } else {
+            if ($DBtable == 'sub_franchisee') {
+                $initial_inv = $row['amount'];
+            }
             $customer_type= $DBtable == 'ca_customer'?$row['customer_type']:'';
             $rd = new DateTime($row['register_date']);
             $rdate = $rd->format('d-m-Y');
@@ -578,6 +585,12 @@ $User_name = ($DBtable == 'business_developement_manager' || $DBtable == 'busine
                                     <?php 
                                         } 
                                     ?>
+                                    <?php if ($DBtable == 'sub_franchisee') { ?>
+                                        <li class="nav-item">
+                                            <a class="nav-link" data-bs-toggle="tab" role="tab" href="#s_p">Upgrade History</a>
+                                        </li>
+                                    <?php } ?>
+
                                 </ul>
                             </nav>
                         </div>
@@ -9606,8 +9619,138 @@ $User_name = ($DBtable == 'business_developement_manager' || $DBtable == 'busine
                                         </tbody>
                                     </table>
                                 </div>
+                                
                             </div>
                             <!-- Payout End -->
+                            <?php 
+                                if($DBtable == 'sub_franchisee'){
+                            ?>
+                            <!-- upgarde History Start -->
+                            <div class="tab-pane fade card px-3 rounded-4" id="s_p" role="tabpanel">
+                                <div class="row">
+                                    <div class="d-flex justify-content-end">
+                                        <div class="pt-3 pb-2 col-md-7">
+                                            <h5>upgarde History</h5>
+                                        </div>
+                                        <?php
+                                            $sql101= "SELECT old_investment_amt,new_investment_amt,upgrade_amt FROM sub_franchisee_upgrade
+                                                                WHERE sub_franchisee_id='".$id."' and upgrade_status=1
+                                                                ORDER BY upgrade_request_date DESC ";
+                                            $stmt101 = $conn->prepare($sql101);
+                                            //print_r($stmt101);
+                                            $stmt101->execute();
+                                            $stmt101->setFetchMode(PDO::FETCH_ASSOC);
+                                            if ($stmt101->rowCount() > 0) {
+                                                 foreach (($stmt101->fetchAll()) as $key => $row) {
+                                                    $tamount = $row['upgrade_amt'];
+                                                 }
+                                            }else{
+                                                $tamount = $initial_inv;
+                                            }
+                                        ?>
+                                        <div class="pt-3 pb-2 col-md-5">
+                                            <div class="row justify-content-end">
+                                                <div class="col-md-4 d-flex align-items-center justify-content-end gap-2">
+                                                    <span class="fw-semibold">Total Investment:</span>
+                                                    <span class="badge bg-success fs-6 px-3 py-2">
+                                                        <?= htmlspecialchars($tamount, ENT_QUOTES, 'UTF-8') ?>
+                                                    </span>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Table -->
+                                <div class="table-responsive table-desi pb-2" id="filterTable1">
+                                    <!-- table roe limit -->
+                                    <table class="table table-hover" id="upgardeHistoryTable">
+                                        <thead>
+                                            <tr>
+                                                <th class="ceterText fw-semibold fs-6">Investment Date</th>
+                                                <th class="ceterText fw-semibold fs-6">Invested Amount</th>
+                                                <th class="ceterText fw-semibold fs-6">Commission Percentage</th>
+                                                <th class="ceterText fw-semibold fs-6">incentive Percentage</th>
+                                                <th class="ceterText fw-semibold fs-6">payment mode</th>
+                                                <th class="ceterText fw-semibold fs-6">note</th>
+                                                <th class="ceterText fw-semibold fs-6">Approved By</th>
+                                                <th class="ceterText fw-semibold fs-6">Approved date</th>
+                                                <th class="ceterText fw-semibold fs-6">Remark</th>
+                                                <th class="ceterText fw-semibold fs-6">Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody id="upgardeHistory">
+                                            <?php
+                                            
+                                            $sqlUnion = "SELECT new_investment_amt,upgrade_amt,upgrade_request_date,upgrade_approval_date,new_commission_per,new_incentive_per,
+                                                                payment_mode,cheque_no,cheque_date,bank_name,transaction_no,payment_proof,rejection_reason,
+                                                                approved_by,note,upgrade_status 
+                                                                FROM sub_franchisee_upgrade
+                                                                WHERE sub_franchisee_id='".$id."'
+                                                                ORDER BY upgrade_request_date ASC ";
+                                            $stmtUnion = $conn->prepare($sqlUnion);
+                                            $stmtUnion->execute();
+                                            $stmtUnion->setFetchMode(PDO::FETCH_ASSOC);
+                                            if ($stmtUnion->rowCount() > 0) {
+                                                foreach (($stmtUnion->fetchAll()) as $key => $row) {
+                                                    $ud = new DateTime($row['upgrade_request_date']);
+                                                    $udate = $ud->format('d-m-Y');
+                                                    $ad = new DateTime($row['upgrade_approval_date']);
+                                                    $adate = $ad->format('d-m-Y');
+
+                                                    // replace dot at end of the line with break statement
+                                                    // $message = $row['message'];
+                                                    // $message1 =  str_replace('.','<br>',$message1); 
+
+                                                    $tamount = $row['upgrade_amt'];
+                                                    $amount = $row['new_investment_amt'];
+                                                    $comm = $row['new_commission_per'];
+                                                    $inc = $row['new_incentive_per'];
+                                                    $pay_mode = $row['payment_mode'];
+                                                    $aproved_by = $row['approved_by'];
+                                                    $note = $row['note'];
+                                                    $rejection_reason = trim($row['rejection_reason'] ?? '');
+
+                                                    if ($rejection_reason === '') {
+                                                        $rejection_reason = 'NA';
+                                                    }
+                                                    $status = $row['upgrade_status'];
+                                                    echo '<tr>
+                                                                <td>' . $udate . '</td>
+                                                                <td>' . $amount . '</td>
+                                                                <td>' . $comm . '</td>
+                                                                <td>' . $inc . '</td>
+                                                                <td>' . $pay_mode . '</td>
+                                                                <td style="width: 350px;">' . $note . '</td>
+                                                                <td>' . $aproved_by . '</td>
+                                                                <td>' . $adate . '</td>
+                                                                <td>' . $rejection_reason . '</td>
+                                                                <td>';
+                                                    if ($status == 0) {
+                                                        echo '<span class="badge badge-pill badge-soft-info font-size-10 fw-bold ms-4">Requested</span>';
+                                                    }
+                                                    if ($status == 1) {
+                                                        echo '<span class="badge badge-pill badge-soft-success font-size-10 fw-bold ms-4">Approved</span>';
+                                                    }
+                                                    if ($status == 2) {
+                                                        echo '<span class="badge badge-pill badge-soft-danger font-size-10 fw-bold ms-4">Rejected</span>';
+                                                    }
+                                                    echo '</td>
+                                                            </tr>
+                                                            ';
+                                                }
+                                            }
+                                            
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                
+                            </div>
+                            <!-- upgarde History End -->
+                            <?php
+                                }
+                            ?>
                             <?php 
                                 if($DBtable == 'ca_customer'){
                             ?>
@@ -9934,6 +10077,14 @@ $User_name = ($DBtable == 'business_developement_manager' || $DBtable == 'busine
         }
     </script>
     <script>
+        $('#upgardeHistoryTable').DataTable({
+            paging: true,
+            searching: true,
+            ordering: true,
+            info: true,
+            lengthChange: true,
+            pageLength: 10
+        });
         var acc = document.getElementsByClassName("accordion");
         var i;
 
