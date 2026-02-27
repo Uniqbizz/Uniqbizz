a428c224677c6d3c284cd9f1b40ce92a0e34dff6
--global <saviovaz20@gmail.com>
2026-02-27 16:09:52 +0530

    inifinite loging routing loop fix

--- admin/login.php
+++ admin/login.php
@@ -1,8 +1,5 @@
 <?php
-    session_start();
-    if (!isset($_SESSION['username'])) {
-        echo '<script>location.href = "login.php";</script>';
-    }
+    
     require 'connect.php';
     $date = date('Y');
 ?>
