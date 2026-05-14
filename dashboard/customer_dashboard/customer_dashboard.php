ff89bf4c381294bb792aa1fc8a42aee2d4789518
nishantcm <cmnishantwork@gmail.com>
2026-05-14 17:31:05 +0530

    1.2 Created, Add and Updated the customer dashboard section 3

--- dashboard/customer_dashboard/customer_dashboard.php
+++ dashboard/customer_dashboard/customer_dashboard.php
@@ -351,6 +351,82 @@
                                 </div>
                             </div>
                         </div>
+                        <!-- card section 3 -->
+                        <div class="card border border-2 rounded-4 my-3">
+                            <div class="row">
+                                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12">
+                                    <img src="../assets/images/medal.png" alt="Medal" class="img-fluid w-100 medal">
+                                </div>
+                                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 premiumCard">
+                                    <h4 class="textColor fw-bolder my-4 d-flex">Premium Customer Membership 
+                                        <div class="premiumIcon ms-3">
+                                            <i class="fa-solid fa-web-awesome me-1" style="color: #e0a10d;"></i>Premium
+                                        </div>
+                                    </h4>
+                                    <div class="row">
+                                        <div class="col-lg-3 col-md-3 col-sm-3 col-3 d-flex align-items-center border-end">
+                                            <div>
+                                                <div class="d-flex justify-content-center align-items-center">
+                                                    <div class="membershipIcon">
+                                                        <i class="ri-plane-line"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="mt-4">
+                                                    <p class="text-muted mb-1 membershipTitle">One-time Payment</p>
+                                                    <p class="mb-3 fw-bolder text-center textColor membershipBold">&#8377;30,000</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-sm-3 col-3 d-flex align-items-center border-end">
+                                            <div>
+                                                <div class="d-flex justify-content-center align-items-center">
+                                                    <div class="membershipIcon">
+                                                        <i class="ri-ticket-2-line"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="mt-4">
+                                                    <p class="text-muted mb-1 membershipTitle">10 Holiday Vouchers</p>
+                                                    <p class="mb-3 fw-bolder text-center textColor membershipBold">Worth &#8377;3,000 each</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-sm-3 col-3 d-flex align-items-center border-end">
+                                            <div>
+                                                <div class="d-flex justify-content-center align-items-center">
+                                                    <div class="membershipIcon">
+                                                        <i class="ri-plane-line"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="mt-4">
+                                                    <p class="text-muted mb-1 membershipTitle">Complimentary</p>
+                                                    <p class="mb-3 fw-bolder text-center textColor membershipBold">Europe Trip</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-3 col-md-3 col-sm-3 col-3 d-flex align-items-center border-end">
+                                            <div>
+                                                <div class="d-flex justify-content-center align-items-center">
+                                                    <div class="membershipIcon">
+                                                        <i class="fa-solid fa-gem"></i>
+                                                    </div>
+                                                </div>
+                                                <div class="mt-4">
+                                                    <p class="text-muted mb-1 membershipTitle">Exclusive Privileges</p>
+                                                    <p class="mb-3 fw-bolder text-center textColor membershipBold">& much more</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-12 col-12 d-flex justify-content-center align-items-center px-3">
+                                    <a href="#">
+                                        <div class="linkBtn p-3 border border-primary border-2 mb-3">
+                                            <p class="fs-6 mb-0 fw-bolder"> View Membership Details</p>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <?php include_once "customer_footer.php" ?>
