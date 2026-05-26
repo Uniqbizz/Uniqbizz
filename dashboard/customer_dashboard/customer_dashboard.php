3c89fece20842fd9c0749d637aa0723018019d7c
zanster97 <saviovaz20@gmail.com>
2026-05-26 12:40:51 +0530

    uniqbizz 1.1 code fix of url on dashboard

--- dashboard/customer_dashboard/customer_dashboard.php
+++ dashboard/customer_dashboard/customer_dashboard.php
@@ -1,7 +1,5 @@
 <?php
-    $base_url = "/ca.uniqbizz.com/dashboard/";
-    $base_url_cust = "/ca.uniqbizz.com/dashboard/customer_dashboard/";
-    $home_url = "/ca.uniqbizz.com/";
+    include (__DIR__.'/urls.php');
     include_once(__DIR__ . '/../dashboard_user_details.php');
     include (__DIR__ .'/customer_model.php');
     include (__DIR__ .'/customer_mapping.php');
