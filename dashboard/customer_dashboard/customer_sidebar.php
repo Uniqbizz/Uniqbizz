7332630b731ce7279d1a2173b05caafc7206d718
zanster97 <saviovaz20@gmail.com>
2026-05-26 12:15:15 +0530

    uniqbizz 1.1 fix of the url code fix

--- dashboard/customer_dashboard/customer_sidebar.php
+++ dashboard/customer_dashboard/customer_sidebar.php
@@ -5,7 +5,7 @@
     $current_page = basename($_SERVER['PHP_SELF']);
 ?>
 <!-- modal css -->
-<link rel="stylesheet" href="<?= $$base_url ?>assets/css/neo_select/my_consultant_modal.css" />
+<link rel="stylesheet" href="<?= $base_url ?>assets/css/neo_select/my_consultant_modal.css" />
 <div class="app-menu navbar-menu rounded-4 bg-white" style="position: fixed; margin-top: 80px !important; width: 240px; padding-top: 0px !important; padding-bottom: 0px !important; margin-bottom: 10px !important;">
     <!-- LOGO -->
     <div class="navbar-brand-box">
@@ -24,7 +24,7 @@
             <ul class="navbar-nav" id="navbar-nav" >
                 <li class="menu-title"><span data-key="t-menu">Menu</span></li>
                 <li class="nav-item <?= ($current_page == 'customer_dashboard.php') ? 'active' : ''; ?>">
-                    <a class="nav-link menu-link" href="<?= $$base_url_cust?>customer_dashboard.php" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
+                    <a class="nav-link menu-link" href="<?= $base_url_cust?>customer_dashboard.php" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
                         <i class="fa-regular fa-house d-flex"></i> <span data-key="t-dashboards">Dashboards</span>
                     </a>
                 </li>
@@ -34,17 +34,17 @@
                     </a>
                 </li>
                 <li class="nav-item <?= ($current_page == 'order_history.php') ? 'active' : ''; ?>">
-                    <a class="nav-link menu-link" href="<?= $$base_url?>order_history.php" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
+                    <a class="nav-link menu-link" href="<?= $base_url?>order_history.php" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
                         <i class="ri-calendar-check-line"></i> <span data-key="t-home">My Bookings</span>
                     </a>
                 </li>
                 <!-- <li class="nav-item <php ($current_page == 'order_history.php') ? 'active' : ''; ?>">
-                    <a class="nav-link menu-link" href="<php $$base_url?>order_history.php" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
+                    <a class="nav-link menu-link" href="<php $base_url?>order_history.php" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
                         <i class="ri-wallet-line"></i> <span data-key="t-home">Upcoming Trips</span>
                     </a>
                 </li> -->
                 <li class="nav-item <?= ($current_page == 'customer_wallet.php') ? 'active' : ''; ?>">
-                    <a class="nav-link menu-link" href="<?= $$base_url_cust.$folder_map[$customer['customer_type']]?>customer_wallet.php" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
+                    <a class="nav-link menu-link" href="<?= $base_url_cust.$folder_map[$customer['customer_type']]?>customer_wallet.php" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
                         <i class="ri-wallet-line"></i> <span data-key="t-home">Wallet</span>
                     </a>
                 </li>
@@ -54,12 +54,12 @@
                     </a>
                 </li> -->
                 <li class="nav-item <?= ($current_page == 'customer_benefit.php') ? 'active' : ''; ?>">
-                    <a class="nav-link menu-link" href="<?= $$base_url_cust.$folder_map[$customer['customer_type']]?>customer_benefit.php" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
+                    <a class="nav-link menu-link" href="<?= $base_url_cust.$folder_map[$customer['customer_type']]?>customer_benefit.php" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
                         <i class="ri-vip-crown-2-line"></i> <span data-key="t-home"><?= $customer['customer_type'] ?> Membership</span>
                     </a>
                 </li>
                 <li class="nav-item <?= ($current_page == 'view_customer.php') ? 'active' : ''; ?>">
-                    <a class="nav-link menu-link" href="<?= $$base_url?>view_customer.php" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
+                    <a class="nav-link menu-link" href="<?= $base_url?>view_customer.php" role="button" aria-expanded="false" aria-controls="sidebarDashboards">
                         <i class="ri-group-line"></i> <span data-key="t-home">Refer & Earn</span>
                     </a>
                 </li>
@@ -83,12 +83,13 @@
                     </a>
                 </li>
                 <li class="nav-item <?= ($current_page == 'profile.php') ? 'active' : ''; ?>">
-                    <a class="nav-link menu-link" href="<?= $$base_url?>profile.php">
+                    <a class="nav-link menu-link" href="<?= $base_url?>profile.php">
                         <i class="ri-settings-3-line"></i>
                         <span>Settings</span>
                     </a>
                 </li>
-                <img src="<?= $$base_url?>assets/images/sidebarImage.png" alt="Package" class="sidebarImage">
+                dashboard\assets\images\sidebarImage.png
+                <img src="<?= $base_url?>assets/images/sidebarImage.png" alt="Package" class="sidebarImage">
                 <div class="exploreCard">
                     <h3 class="fw-bold text-white">Dream. <br> <span class="text-warning">Explore.</span> <br> <span class="text-warning">Discover.</span></h3>
                     <p class="text-white">Your next adventure is just a click away.</p>
@@ -100,7 +101,7 @@
                     </a>
                 </div>
                 <li class="nav-item <?= ($current_page == 'customer_benefit.php') ? 'active' : ''; ?>">
-                    <a class="logoutBtn mylogout" href="<?= $$base_url?>logout.php">
+                    <a class="logoutBtn mylogout" href="<?= $base_url?>logout.php">
                         <i class="mdi mdi-logout d-flex justify-content-start align-items-center me-3"></i>
                         <span class="fs-5 mb-0 fw-bolder" data-key="t-logout">Logout</span>
                     </a>
