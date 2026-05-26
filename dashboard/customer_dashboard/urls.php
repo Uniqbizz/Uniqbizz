ac32fee6bfe433743dd4aecc8bb6ea1d4d4fce14
zanster97 <saviovaz20@gmail.com>
2026-05-26 12:37:03 +0530

    uniqbizz 1.1 git url fix for git.uniqbizz

--- dashboard/customer_dashboard/urls.php
+++ dashboard/customer_dashboard/urls.php
@@ -1,5 +1,9 @@
 <?php
-    $base_url = "/ca.uniqbizz.com/dashboard/";
-    $base_url_cust = "/ca.uniqbizz.com/dashboard/customer_dashboard/";
-    $home_url = "/ca.uniqbizz.com/";
+    // $base_url = "/ca.uniqbizz.com/dashboard/";
+    // $base_url_cust = "/ca.uniqbizz.com/dashboard/customer_dashboard/";
+    // $home_url = "/ca.uniqbizz.com/";
+    //git urls
+    $base_url = "/git.uniqbizz.com/dashboard/";
+    $base_url_cust = "/git.uniqbizz.com/dashboard/customer_dashboard/";
+    $home_url = "/git.uniqbizz.com/";
 ?>
