60ffa136d463cc7115593c083f44310e6a827ce7
nishantcm <cmnishantwork@gmail.com>
2026-06-03 14:17:42 +0530

    1.3 Updated the files title

--- admin/customer_wallet/discountWallet.php
+++ admin/customer_wallet/discountWallet.php
@@ -10,7 +10,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8" />
-        <title>Referral Wallet | Admin Dashboard </title>
+        <title>Discount Wallet | Admin Dashboard </title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- App favicon -->
         <link rel="shortcut icon" href="../assets/images/fav.png">
