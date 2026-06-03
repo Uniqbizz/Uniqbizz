8446599ccd11faf23de78d17eb6ace3a0b30037e
nishantcm <cmnishantwork@gmail.com>
2026-06-03 12:01:20 +0530

    1.1 Created the extendedWallet.php file for with section1 and Updated the customerWallet.php file added link to extended wallet card

--- admin/customer_wallet/customerWallet.php
+++ admin/customer_wallet/customerWallet.php
@@ -226,7 +226,7 @@
                                 </a>
                             </div>
                             <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 col-12 mb-2">
-                                <a href="#">
+                                <a href="extendedWallet.php">
                                     <div class="rounded-3 px-3 py-2 walletCard2">
                                         <div class="d-flex gap-2">
                                             <div class="walletIcon2">
