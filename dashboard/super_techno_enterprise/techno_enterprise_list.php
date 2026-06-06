f1ab264e32ad9e6f4cb6200f13333177c343e993
nishantcm <cmnishantwork@gmail.com>
2026-06-06 16:52:35 +0530

    1.2 Updated the techno_enterprise_list file with registered list with css file.

--- dashboard/super_techno_enterprise/techno_enterprise_list.php
+++ dashboard/super_techno_enterprise/techno_enterprise_list.php
@@ -250,6 +250,207 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <div class="card rounded-4 border-1">
+                                                <div class="card-header border-bottom-dashed rounded-top-4">
+                                                    <div class="row">
+                                                        <div class="col-xl-6 col-lg-5 col-md-12 col-sm-12 col-12 mb-2">
+                                                            <div class="d-flex gap-3">
+                                                                <div class="tePendingIcon tePendingIcon2">
+                                                                    <i class="ri-verified-badge-line" style="font-size: 30px;"></i>
+                                                                </div>
+                                                                <div class="align-content-end">
+                                                                    <h5 class="card-title text-dark mb-0">Registered Techno Enterprise List</h5>
+                                                                    <p class="text-muted fs-6 mb-0">All approved and active Techno Enterprises</p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-xl-6 col-lg-7 col-md-12 col-sm-12 col-12 mb-2">
+                                                            <div class="row d-flex justify-content-end gap-2 teSectionSize">
+                                                                <div class="col-lg-3 col-md-4 col-sm-4 col-6 mb-2">
+                                                                    <!-- <div> -->
+                                                                        <input type="date" id="date" name="date" class="dateInput">
+                                                                    <!-- </div>   -->
+                                                                </div>
+                                                                <div class="col-lg-4 col-md-4 col-sm-4 col-6 mb-2">
+                                                                    <div class="d-flex gap-2">
+                                                                        <p class="fs-6 text-dark mb-1 align-content-center">Count</p>
+                                                                        <input type="number" class="dateInput" value="50">
+                                                                    </div>
+                                                                </div>
+                                                                <div class="col-lg-4 col-md-4 col-sm-4 col-12 mb-2">
+                                                                    <a href="#" class="text-decoration-none" id="#">
+                                                                        <div class="stWalletBtn rounded-3 py-2 align-items-center justify-content-center justify-content-lg-start">
+                                                                            <i class="fa-solid fa-download me-2"></i>
+                                                                            <p class="fs-6 mb-0 fw-bolder pe-1">Download</p>
+                                                                        </div>
+                                                                    </a>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="card-body">
+                                                    <table id="example-dataTable-2" class="table table-striped table-bordered dt-responsive nowrap table-striped align-middle" style="width:100%">
+                                                        <thead>
+                                                            <tr class="table-primary">
+                                                                <th data-ordering="false">TE ID & Full Name</th>
+                                                                <th data-ordering="false">Reference ID & Name</th>
+                                                                <th data-ordering="false">Phone & Email</th>
+                                                                <th data-ordering="false">Amt (&#8377;)</th>
+                                                                <th data-ordering="false">Joining Date</th>
+                                                                <th data-ordering="false">Status</th>
+                                                                <th data-ordering="false">Action</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <td>
+                                                                    <div class="">
+                                                                        <p class="fs-6 mb-0">Rajesh Kumar</p>
+                                                                        <p class="fs-6 mb-0">STE-REF-00023</p>
+                                                                    </div>
+                                                                    
+                                                                </td>
+                                                                <td>
+                                                                    <div class="">
+                                                                        <p class="fs-6 mb-0">Pandurang Naik</p>
+                                                                        <p class="fs-6 mb-0">STE-REF-00023</p>
+                                                                    </div>
+                                                                </td>
+                                                                <td>
+                                                                    <div class="">
+                                                                        <p class="fs-6 mb-0"><i class="fa-solid fa-phone me-2"></i>+91 9876543210</p>
+                                                                        <p class="fs-6 mb-0"><i class="fa-regular fa-envelope me-2"></i>rajesh.kumar@email.com</p>
+                                                                    </div>
+                                                                </td>
+                                                                <td>
+                                                                    <p class="fs-6 mb-0"> 50,000</p>
+                                                                </td>
+                                                                <td>
+                                                                    <p class="fs-6 mb-0"><i class="fa-solid fa-calendar-days me-2"></i>20 May 2024</p>
+                                                                </td>
+                                                                <td>
+                                                                    <p class="teActiveBtn rounded-pill text-center">Active</p>
+                                                                </td>
+                                                                <td>
+                                                                    <a href="#">
+                                                                        <p class="teViewBtn text-center fw-bold"><i class="fa-solid fa-eye me-2 mt-1"></i>View</p>
+                                                                    </a>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>
+                                                                    <div class="">
+                                                                        <p class="fs-6 mb-0">Rajesh Kumar</p>
+                                                                        <p class="fs-6 mb-0">STE-REF-00023</p>
+                                                                    </div>
+                                                                    
+                                                                </td>
+                                                                <td>
+                                                                    <div class="">
+                                                                        <p class="fs-6 mb-0">Pandurang Naik</p>
+                                                                        <p class="fs-6 mb-0">STE-REF-00023</p>
+                                                                    </div>
+                                                                </td>
+                                                                <td>
+                                                                    <div class="">
+                                                                        <p class="fs-6 mb-0"><i class="fa-solid fa-phone me-2"></i>+91 9876543210</p>
+                                                                        <p class="fs-6 mb-0"><i class="fa-regular fa-envelope me-2"></i>rajesh.kumar@email.com</p>
+                                                                    </div>
+                                                                </td>
+                                                                <td>
+                                                                    <p class="fs-6 mb-0"> 50,000</p>
+                                                                </td>
+                                                                <td>
+                                                                    <p class="fs-6 mb-0"><i class="fa-solid fa-calendar-days me-2"></i>20 May 2024</p>
+                                                                </td>
+                                                                <td>
+                                                                    <p class="teActiveBtn rounded-pill text-center">Active</p>
+                                                                </td>
+                                                                <td>
+                                                                    <a href="#">
+                                                                        <p class="teViewBtn text-center fw-bold"><i class="fa-solid fa-eye me-2 mt-1"></i>View</p>
+                                                                    </a>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>
+                                                                    <div class="">
+                                                                        <p class="fs-6 mb-0">Rajesh Kumar</p>
+                                                                        <p class="fs-6 mb-0">STE-REF-00023</p>
+                                                                    </div>
+                                                                    
+                                                                </td>
+                                                                <td>
+                                                                    <div class="">
+                                                                        <p class="fs-6 mb-0">Pandurang Naik</p>
+                                                                        <p class="fs-6 mb-0">STE-REF-00023</p>
+                                                                    </div>
+                                                                </td>
+                                                                <td>
+                                                                    <div class="">
+                                                                        <p class="fs-6 mb-0"><i class="fa-solid fa-phone me-2"></i>+91 9876543210</p>
+                                                                        <p class="fs-6 mb-0"><i class="fa-regular fa-envelope me-2"></i>rajesh.kumar@email.com</p>
+                                                                    </div>
+                                                                </td>
+                                                                <td>
+                                                                    <p class="fs-6 mb-0"> 50,000</p>
+                                                                </td>
+                                                                <td>
+                                                                    <p class="fs-6 mb-0"><i class="fa-solid fa-calendar-days me-2"></i>20 May 2024</p>
+                                                                </td>
+                                                                <td>
+                                                                    <p class="teActiveBtn rounded-pill text-center">Active</p>
+                                                                </td>
+                                                                <td>
+                                                                    <a href="#">
+                                                                        <p class="teViewBtn text-center fw-bold"><i class="fa-solid fa-eye me-2 mt-1"></i>View</p>
+                                                                    </a>
+                                                                </td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td>
+                                                                    <div class="">
+                                                                        <p class="fs-6 mb-0">Rajesh Kumar</p>
+                                                                        <p class="fs-6 mb-0">STE-REF-00023</p>
+                                                                    </div>
+                                                                    
+                                                                </td>
+                                                                <td>
+                                                                    <div class="">
+                                                                        <p class="fs-6 mb-0">Pandurang Naik</p>
+                                                                        <p class="fs-6 mb-0">STE-REF-00023</p>
+                                                                    </div>
+                                                                </td>
+                                                                <td>
+                                                                    <div class="">
+                                                                        <p class="fs-6 mb-0"><i class="fa-solid fa-phone me-2"></i>+91 9876543210</p>
+                                                                        <p class="fs-6 mb-0"><i class="fa-regular fa-envelope me-2"></i>rajesh.kumar@email.com</p>
+                                                                    </div>
+                                                                </td>
+                                                                <td>
+                                                                    <p class="fs-6 mb-0"> 50,000</p>
+                                                                </td>
+                                                                <td>
+                                                                    <p class="fs-6 mb-0"><i class="fa-solid fa-calendar-days me-2"></i>20 May 2024</p>
+                                                                </td>
+                                                                <td>
+                                                                    <p class="teActiveBtn rounded-pill text-center">Active</p>
+                                                                </td>
+                                                                <td>
+                                                                    <a href="#">
+                                                                        <p class="teViewBtn text-center fw-bold"><i class="fa-solid fa-eye me-2 mt-1"></i>View</p>
+                                                                    </a>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
