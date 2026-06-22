e80157bb9e18d4042e2f51c88f9014ff93a5e7e3
nishantcm <cmnishantwork@gmail.com>
2026-06-22 17:24:15 +0530

    1.2 Created the dashboard, footer, header, and sidebar files

---
+++ dashboard/travel_consultant/travel_consultant_sidebar.php
@@ -0,0 +1,82 @@
+<?php
+    $current_page = basename($_SERVER['PHP_SELF']);
+?>
+<div class="app-menu navbar-menu rounded-4 bg-white" style="position: fixed; margin-top: 80px !important; width: 240px; padding-top: 0px !important; padding-bottom: 0px !important; margin-bottom: 10px !important;">
+    <!-- LOGO -->
+    <div class="navbar-brand-box">
+        
+        <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
+            <i class="ri-record-circle-line"></i>
+        </button>
+    </div>
+    
+    <!-- sidebar -->
+    <div id="scrollbar" >
+        <div class="container-fluid">
+
+            <div id="two-column-menu">
+            </div>
+            <ul class="navbar-nav" id="navbar-nav" >
+                <li class="menu-title"><span data-key="t-menu">Menu</span></li>
+                <li class="nav-item <?php echo ($current_page == 'travel_consultant_dashboard.php') ? 'active' : ''; ?>">
+                    <a class="nav-link menu-link" href="travel_consultant_dashboard.php">
+                        <i class="fa-regular fa-house d-flex"></i><span>Dashboards</span>
+                    </a>
+                </li>
+                <li class="nav-item <?php echo ($current_page == '../../tour-list.php') ? 'active' : ''; ?>">
+                    <a class="nav-link menu-link" href="../../tour-list.php">
+                        <i class="ri-user-shared-2-line"></i> <span>Explore Packages</span>
+                    </a>
+                </li>
+                <li class="nav-item <?php echo ($current_page == 'order_history.php') ? 'active' : ''; ?>">
+                    <a class="nav-link menu-link" href="order_history.php">
+                        <i class="ri-user-shared-2-line"></i> <span data-key="t-home">My Bookings</span>
+                    </a>
+                </li>
+                <li class="nav-item <?php echo ($current_page == '#') ? 'active' : ''; ?>">
+                    <a class="nav-link menu-link" href="#">
+                        <i class="ri-user-shared-2-line"></i> <span data-key="t-home">Upcoming Trips</span>
+                    </a>
+                </li>
+                <li class="nav-item <?php echo ($current_page == '#') ? 'active' : ''; ?>">
+                    <a class="nav-link menu-link" href="#">
+                        <i class="ri-user-shared-2-line"></i> <span data-key="t-home">Holiday Account</span>
+                    </a>
+                </li>
+                <li class="nav-item <?php echo ($current_page == '#') ? 'active' : ''; ?>">
+                    <a class="nav-link menu-link" href="#">
+                        <i class="ri-user-shared-2-line"></i> <span data-key="t-home">Support</span>
+                    </a>
+                </li>
+                <li class="nav-item <?php echo ($current_page == '#') ? 'active' : ''; ?>">
+                    <a class="nav-link menu-link" href="#">
+                        <i class="ri-user-shared-2-line"></i> <span data-key="t-home">Settings</span>
+                    </a>
+                </li>
+                <img src="../assets/images/sidebarImage.png" alt="Package" class="sidebarImage">
+                <div class="exploreCard">
+                    <h3 class="fw-bold text-white">Dream. <br> <span class="text-warning">Explore.</span> <br> <span class="text-warning">Discover.</span></h3>
+                    <p class="text-white">Your next adventure is just a click away.</p>
+                    <a href="../../tour-list.php">
+                        <div class="exploreBtn">
+                            <p class="fs-5 mb-0 fw-bolder">Explore Packages</p>
+                            <i class="ri-arrow-right-line d-flex align-items-center"></i>
+                        </div>
+                    </a>
+                </div>
+                <li class="nav-item <?php echo ($first_part=="../logout.php") ? "active" : ""; ?>">
+                    <a class="logoutBtn mylogout" href="../logout.php">
+                        <i class="mdi mdi-logout d-flex justify-content-start align-items-center me-3"></i>
+                        <span class="fs-5 mb-0 fw-bolder" data-key="t-logout">Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <!-- Sidebar -->
+    </div>
+
+    <div class="sidebar-background"></div>
+</div>
+<!-- Left Sidebar End -->
+
+<!-- vertical-overlay -->
