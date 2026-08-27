95cc6c7a7e478fcac84e3e2f38d7f50cf8eb0bb0
nishantcm <cmnishantwork@gmail.com>
2026-08-27 16:30:13 +0530

    1.2 Created the enquiries.php file with enquiries.css file for new design.

---
+++ admin/enquiries/enquiries.php
@@ -0,0 +1,220 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    echo '<script>location.href = "../login.php";</script>';
+}
+$date = date('Y'); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Enquiries</title>
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="../assets/images/fav.png">
+        <!-- custom css file -->
+        <!-- <link href="../assets/css/styles.css" rel="stylesheet" type="text/css" /> -->
+        <!-- Bootstrap Css -->
+        <link href="../assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="../assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="../assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+        <!-- Css-->
+        <link href="../assets/css/loadingScreen.css" id="app-style" rel="stylesheet" type="text/css" />
+        <!-- Enquiries Css-->
+        <link href="../assets/css/enquiries.css" id="app-style" rel="stylesheet" type="text/css" />
+        <!-- App js -->
+        <!-- <script src="assets/js/plugin.js"></script> -->
+        <!-- DataTables -->
+        <link href="../assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+
+        <!-- Responsive datatable examples -->
+        <link href="../assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+        <!-- Font Awesome Icons -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- Date Range Picker CSS Start -->
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+        <!-- Date Range Picker CSS End -->
+    </head>
+    <body data-sidebar="dark">
+        <div class="layout-wrapper">
+            <?php
+                // top header logo, hamberger menu, fullscreen icon, profile
+                include_once '../header.php';
+
+                // sidebar navigation menu 
+                include_once '../sidebar.php';
+
+                $today = date('Y-m-d'); // Get today's date as a string
+
+                $mindate= "01-01-2022";
+                $maxdate=$today;
+            ?>
+            <div class="main-content">
+                <div class="page-content">
+                    <div class="container-fluid">
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                                    <h4 class="mb-sm-0 font-size-18">Enquiries</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end page title -->
+                        <div class="row rowAlignment">
+                            <div class="cardWidth">
+                                <div class="card rounded-4 p-3 cardHeight">
+                                    <div class="d-flex align-items-center">
+                                        <span class="cardHover cardHover1">
+                                            <i class="fa-solid fa-user-plus fa-xl faIcon"></i>
+                                        </span>
+                                        <div class="ms-4">
+                                            <h6 class="fontSize12 mb-1">New Enquiries</h6>
+                                            <p class="fs-4 fw-bold mb-0">28</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="cardWidth">
+                                <div class="card rounded-4 p-3 cardHeight">
+                                    <div class="d-flex align-items-center">
+                                        <span class="cardHover cardHover2">
+                                            <i class="fa-solid fa-spinner fa-xl faIcon"></i>
+                                        </span>
+                                        <div class="ms-4">
+                                            <h6 class="fontSize12 mb-1">In Progress</h6>
+                                            <p class="fs-4 fw-bold mb-0">28</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="cardWidth">
+                                <div class="card rounded-4 p-3 cardHeight">
+                                    <div class="d-flex align-items-center">
+                                        <span class="cardHover cardHover3">
+                                            <i class="fa-solid fa-paper-plane fa-xl faIcon"></i>
+                                        </span>
+                                        <div class="ms-4">
+                                            <h6 class="fontSize12 mb-1">Quotation Sent</h6>
+                                            <p class="fs-4 fw-bold mb-0">28</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="cardWidth">
+                                <div class="card rounded-4 p-3 cardHeight">
+                                    <div class="d-flex align-items-center">
+                                        <span class="cardHover cardHover4">
+                                            <i class="fa-solid fa-clock fa-xl faIcon"></i>
+                                        </span>
+                                        <div class="ms-4">
+                                            <h6 class="fontSize12 mb-1">Awaiting Response</h6>
+                                            <p class="fs-4 fw-bold mb-0">28</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="cardWidth">
+                                <div class="card rounded-4 p-3 cardHeight">
+                                    <div class="d-flex align-items-center">
+                                        <span class="cardHover cardHover5">
+                                            <i class="fa-solid fa-circle-check fa-xl faIcon"></i>
+                                        </span>
+                                        <div class="ms-4">
+                                            <h6 class="fontSize12 mb-1">Closed</h6>
+                                            <p class="fs-4 fw-bold mb-0">28</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+                <?php include_once "../footer.php" ?>
+            </div>
+        </div>
+        <!-- END layout-wrapper -->
+        <!--start back-to-top-->
+        <button onclick="topFunction()" class="scrollToTop scroll-btn show btn" id="back-to-top">
+            <i class="mdi mdi-arrow-up"></i>
+        </button>
+        <!--end back-to-top-->
+        <!-- JAVASCRIPT -->
+        <script src="../assets/libs/jquery/jquery.min.js"></script>
+        <script src="../assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="../assets/libs/metismenu/metisMenu.min.js"></script>
+        <script src="../assets/libs/simplebar/simplebar.min.js"></script>
+        <script src="../assets/libs/node-waves/waves.min.js"></script>
+        <!-- Required datatable js -->
+        <script src="../assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+        <script src="../assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+
+        
+        <!-- Responsive examples -->
+        <script src="../assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+        <script src="../assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
+        <!-- Calendar init -->
+        <script src="../assets/libs/fullcalendar/index.global.min.js"></script>
+
+        <!-- Date Range Picker Script Start -->
+        <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script> -->
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+        <!-- Date Range Picker Script End -->
+
+        <!-- App js -->
+        <script src="../assets/js/app.js"></script>
+        <script>
+            var mybutton = document.getElementById("back-to-top");
+
+            function scrollFunction() {
+                100 < document.body.scrollTop || 100 < document.documentElement.scrollTop ? mybutton.style.display = "block" : mybutton.style.display = "none"
+            }
+
+            function topFunction() {
+                document.body.scrollTop = 0,
+                    document.documentElement.scrollTop = 0
+            }
+            mybutton && (window.onscroll = function() {
+                scrollFunction()
+            });
+        </script>
+        <!-- Date Range Script -->
+        <script type="text/javascript">
+            $(function() {
+
+                // var start = moment().subtract(29, 'days');
+                // var end = moment();
+                var start = moment("<?= $mindate ?>", "YYYY-MM-DD");
+                var end = moment("<?= $maxdate ?>", "YYYY-MM-DD");
+
+                function cb(start, end) {
+                    $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+                }
+
+                $('#reportrange').daterangepicker({
+                    startDate: start,
+                    endDate: end,
+                    ranges: {
+                        'Today': [moment(), moment()],
+                        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                        'This Month': [moment().startOf('month'), moment().endOf('month')],
+                        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                    }
+                }, cb);
+
+                cb(start, end);
+
+            });
+        </script>
+        <!-- Date Range Script -->
+    </body>
+</html>
