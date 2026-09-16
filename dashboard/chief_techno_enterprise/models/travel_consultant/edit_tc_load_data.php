b010523b36d81be7f3149a90146a2fbd829d9571
zanster97 <saviovaz20@gmail.com>
2026-09-16 17:34:01 +0530

    more fixes of cu and tc logiv in CTE login

--- dashboard/chief_techno_enterprise/models/travel_consultant/edit_tc_load_data.php
+++ dashboard/chief_techno_enterprise/models/travel_consultant/edit_tc_load_data.php
@@ -3,8 +3,8 @@
 
     header('Content-Type: application/json');
 
-    $id = $_GET['id'] ?? '';
-    $edittype = $_GET['edittype'] ?? '';
+    $id = $_POST['id'] ?? '';
+    $edittype = $_POST['tc_type'] ?? '';
 
     if (empty($id) || empty($edittype)) {
         echo json_encode([
