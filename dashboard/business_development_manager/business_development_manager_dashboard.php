1fb91ab89ad88006603b1f93c464e1fcc6bcc346
zanster97 <saviovaz20@gmail.com>
2026-09-17 14:44:36 +0530

    recent activity fix and view page in BDM login

--- dashboard/business_development_manager/business_development_manager_dashboard.php
+++ dashboard/business_development_manager/business_development_manager_dashboard.php
@@ -321,10 +321,9 @@
                                         <p class="commission-title fs-5 mb-0">
                                             Recent Activities
                                         </p>
-                                        <!-- 
-                                        <a href="#" class="fs-6 fw-bold">
+                                        <a href="recent_activities.php" class="fs-6 fw-bold">
                                             View All
-                                        </a> -->
+                                        </a>
 
                                     </div>
 
